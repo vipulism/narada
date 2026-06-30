39214ca2089e81857d7623e1b8c88e5f55572bc0
Vipulism <vipul0809@gmail.com>
2026-06-30 23:44:18 +0530

    db connection fixed

--- src/schedulers/import.scheduler.ts
+++ src/schedulers/import.scheduler.ts
@@ -2,17 +2,19 @@ import cron from "node-cron";
 import { FolderConnector } from "../connectors/folder/folder.connector";
 import { SmsImportService } from "../importers/sms/smsImport.service";
 
-const smsFolderConnector = new FolderConnector(
-  {
-    name: "SMS Imports",
-    path: "/imports/sms",
-    pattern: /\.xml$/,
-  },
-  new SmsImportService()
-);
 
 export async function startImportScheduler(): Promise<void> {
   console.info("📂 Starting Import Scheduler...");
+
+  const smsFolderConnector = await new FolderConnector(
+    {
+      name: "SMS Imports",
+      path: "/imports/sms",
+      pattern: /\.xml$/,
+    },
+    new SmsImportService()
+  );
+  
 
   // Run once on startup
   await smsFolderConnector.scan();
